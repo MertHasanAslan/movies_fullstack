981be29a44fff94787e36b28f0e84962602a944a
MertHasanAslan <merthasanaslan325@gmail.com>
2025-11-10 18:34:27 +0300

    Created database by using MongoDB and connected it to project.

---
+++ src/main/java/com/ZorroMovies/movies/movie.java
@@ -0,0 +1,26 @@
+package com.ZorroMovies.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Document(collection = "movies")
+public class movie {
+    private ObjectId id;
+
+    private String imdbId;
+
+    private String title;
+
+    private String releaseDate;
+
+    private String trailerLink;
+
+    private String poster;
+
+    private List<String> genres;
+
+    private List<String> backdrops;
+    
+}
