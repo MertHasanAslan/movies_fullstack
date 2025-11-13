a58db3879eed8f1644232e76f06268d88eae64db
MertHasanAslan <merthasanaslan325@gmail.com>
2025-11-13 20:47:15 +0300

    now u can search for single movie in the database

--- src/main/java/com/ZorroMovies/movies/movieController.java
+++ src/main/java/com/ZorroMovies/movies/movieController.java
@@ -1,18 +1,31 @@
 package com.ZorroMovies.movies;
 
 
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/movies")
 public class movieController {
+    @Autowired
+    private movieService movie_service;
 
     @GetMapping("/get")
-    public ResponseEntity<String> get_all_movies(){
-        return new ResponseEntity<String>("All Movies", HttpStatus.OK);
+    public ResponseEntity<List<movie>> get_all_movies(){
+        return new ResponseEntity<List<movie>>(movie_service.all_movies(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<movie>> get_single_movie(@PathVariable ObjectId id){
+        return new ResponseEntity<Optional<movie>>(movie_service.single_movie(id), HttpStatus.OK);
     }
 }
--- src/main/java/com/ZorroMovies/movies/movieService.java
+++ src/main/java/com/ZorroMovies/movies/movieService.java
@@ -1,14 +1,23 @@
 package com.ZorroMovies.movies;
 
 
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class movieService {
+    @Autowired
     private movieRepository movie_repository;
-    public List<movie> all_movies(){
 
+    public List<movie> all_movies(){
+        return movie_repository.findAll();
+    }
+
+    public Optional<movie> single_movie(ObjectId id){
+        return movie_repository.findById(id);
     }
 }
